44c117f833865c59360337bd05099c629629b86e
Abdul Rahman Rehmtulla <abdul.r.zaid@gmail.com>
2025-06-23 15:34:24 +0300

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>here</>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Jireh Frontend Assessment
+      </h1>
+    </>
+  )
 }
 
 export default App
